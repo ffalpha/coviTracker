f5f35c46b2a5e4956e68398254c89685534f6e3b
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-19 12:59:48 +0530

    Added props

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import { fetchData } from "./api/index";
 import style from "./App.css";
 function App() {
   const [data, setData] = useState({});
-
   useEffect(async () => {
     const doc = await fetchData();
     setData(doc);
   }, []);
-  console.log(data);
+
   return (
     <div className="App container">
-      <Cards />
+      <Cards data={data} />
       <CountryPicker />
       <Chart />
     </div>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Cards() {
+function Cards(props) {
+  console.log(props);
   return <h1>Cards</h1>;
 }
 
