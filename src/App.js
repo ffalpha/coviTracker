a43e95012492b5d1dea40010e10b57aa1b92bad0
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-19 12:34:47 +0530

    Added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Cards, Chart, CountryPicker } from "./components";
+import style from "./App.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container">
+      <Cards />
+      <CountryPicker />
+      <Chart />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CountryPicker() {
+  return <div>Countr</div>;
+}
+
+export default CountryPicker;
