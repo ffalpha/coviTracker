3d1f0af17df610e8f122e2b1cd9afe8fb5566522
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-19 20:32:01 +0530

    Almost finished

--- src/components/Charts/Chart.js
+++ src/components/Charts/Chart.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { fetchDailyDate } from "../../api";
 import { Line, Bar } from "react-chartjs-2";
 import styles from "./Chart.css";
-function Chart() {
+function Chart({ data: { confirmed, deaths, recovered }, country }) {
   const [dailyData, setDailyData] = useState([]);
-
+  console.log(country);
   //getting data
   useEffect(() => {
     async function getDailydata() {
@@ -35,10 +35,31 @@ function Chart() {
         ],
       }}
     />
-  ) : (
-    <div>sadsaads</div>
-  );
-  return <div className="containerchart">{lineChart}</div>;
+  ) : null;
+
+  const barChar = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current State in ${country}` },
+      }}
+    />
+  ) : null;
+  return <div className="containerchart">{country ? barChar : lineChart}</div>;
 }
 
 export default Chart;
