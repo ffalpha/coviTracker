ab8b5ae6127a7b0123325fc52aea9cd7ab60cb75
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-19 13:08:22 +0530

    Added Cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,8 +1,56 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 function Cards(props) {
   console.log(props);
-  return <h1>Cards</h1>;
+  return (
+    <div className="container">
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+
+            <Typography color="textSecondary">Date</Typography>
+
+            <Typography variant="body2">
+              Number of active case of COIVD-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Reacovered
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+
+            <Typography color="textSecondary">Date</Typography>
+
+            <Typography variant="body2">
+              Number of active case of COIVD-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+
+            <Typography color="textSecondary">Date</Typography>
+
+            <Typography variant="body2">
+              Number of daeths case of COIVD-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Cards;
