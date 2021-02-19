8e504d30492837b1acf4f1a4254a06931ff7e82e
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-19 12:48:59 +0530

    Add hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, { useState, useEffect } from "react";
+
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api/index";
 import style from "./App.css";
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(async () => {
+    const doc = await fetchData();
+    setData(doc);
+    return () => console.log("unmounting...");
+  }, [data]);
   return (
     <div className="App container">
       <Cards />
---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+//gettin data
+export const fetchData = async () => {
+  try {
+    const response = await axios.get(url);
+    return response;
+  } catch (error) {}
+};
